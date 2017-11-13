b7e8d5c237f7d86656c8453d267a0a575986f539
User <User@192.168.0.2>
2017-11-12 20:30:52 -0600

    practice with date utility class and list

--- src/practice/practice2.java
+++ src/practice/practice2.java
@@ -3,28 +3,47 @@ package practice;
 
 import edu.wctc.advjava.hch.datetime.DateUtilities;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
 public class practice2 {
     public static void main(String[] args) {
-        LocalDate date1 = LocalDate.now();
-        LocalDate date2 = LocalDate.now().plus(1, ChronoUnit.DAYS);
-        LocalDate date3 = LocalDate.now().minus(7, ChronoUnit.WEEKS);
+        LocalDateTime date1 = LocalDateTime.now();
+        LocalDateTime date2 = LocalDateTime.now().plus(1, ChronoUnit.DAYS);
+        LocalDateTime date3 = LocalDateTime.now().minus(7, ChronoUnit.WEEKS);
         
         DateUtilities dateUtility = new DateUtilities();
         
-        List<LocalDate> dateList = new ArrayList<>();
+        List<LocalDateTime> dateTimeList = new ArrayList<>();
         
-        dateList.add(date1);
-        dateList.add(date2);
-        dateList.add(date3);
+        dateTimeList.add(date1);
+        dateTimeList.add(date2);
+        dateTimeList.add(date3);
+        
+        LocalDateTime today = LocalDateTime.now();
+        
+        for(int i = 0; i < dateTimeList.size(); i++){
+            System.out.println("The difference in days between today and " + dateTimeList.get(i) + " is " + 
+                    dateUtility.differenceBetweenTwoDates(today, dateTimeList.get(i), DateUtilities.unitOfTimeReturnType.DAYS));
+        }
+        
+        LocalDate date4 = LocalDate.now().minus(3, ChronoUnit.DAYS);
+        LocalDate date5 = LocalDate.now().minus(4, ChronoUnit.HOURS);
+        LocalDate date6 = LocalDate.now().plus(6, ChronoUnit.WEEKS);
+        
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(date4);
+        dateList.add(date5);
+        dateList.add(date6);
+        
         
         for (LocalDate d : dateList){
             System.out.println(dateUtility.convertDateToStringDefault(d));
         }
         
-        //convertToStringDefault
+        
+        
     }
 }
